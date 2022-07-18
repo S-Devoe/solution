52b61cd1d6081e0f1bdad4909a3900f7b3e85e31
Lagbalu Lateef <chelseg14@gmail.com>
2022-07-18 22:48:44 +0100

    changes

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,6 +1,7 @@
 const HeroSection = () => {
   return (
     <section className="hero-section">
+      <img src="./hero-desktop.jpg" alt="bg" />
       <p>
         Watch something <span> incredible </span>
       </p>
